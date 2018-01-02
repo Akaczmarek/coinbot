1f35ff22a937eb4da673408d31e3d71b711bd1fb
AlainK <alain.kaczmarek404@gmail.com>
2018-01-02 16:13:21 +0100

    histo ordres liste sorted ok ok ok

--- Angular/ng2-admin/src/app/pages/tables/components/basicTables/components/stripedTable/stripedTable.component.ts
+++ Angular/ng2-admin/src/app/pages/tables/components/basicTables/components/stripedTable/stripedTable.component.ts
@@ -2,21 +2,36 @@ import {Component} from '@angular/core';
 
 import {BasicTablesService} from '../../basicTables.service';
 import {SupervisionService} from '../../../../../../services/supervision.service';
+import { OnInit } from '@angular/core/src/metadata/lifecycle_hooks';
 
 @Component({
   selector: 'striped-table',
   templateUrl: './stripedTable.html'
 })
-export class StripedTable {
+export class StripedTable{
 
-  smartTableData:Array<any>;
+  OrderHistories:Array<any>;
 
   constructor(private supervisionService : SupervisionService ) {
-    //this.smartTableData = supervisionService.mockAchatVenteHisto;
     this.updateOHBUser();
+    }
+
+  
+  updateOHBUser() {
+    this.supervisionService.getOHBbyUser(1).subscribe(reponse => this.OrderHistories = reponse.sort((a: any, b: any) => {
+      let date1 = new Date(a.timestampfinished);
+      let date2 = new Date(b.timestampfinished);
+
+      if (date1 > date2) {
+          return -1;
+      } else if (date1 < date2) {
+          return 1;
+      } else {
+          return 0;
+      }
+  }))
+   
   }
 
-  updateOHBUser() {
-    this.supervisionService.getOHBbyUser(1).subscribe(reponse => this.smartTableData = reponse);
-  }
+  
 }
